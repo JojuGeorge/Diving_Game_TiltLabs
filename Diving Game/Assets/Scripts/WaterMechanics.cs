e5400f2b41669ce3946f2887f5d782543edc6b87
JojuGeorge <jojugeorge001@gmail.com>
2021-10-24 20:33:34 +0530

    under water post processing added

--- Diving Game/Assets/Scripts/WaterMechanics.cs
+++ Diving Game/Assets/Scripts/WaterMechanics.cs
@@ -1,27 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class WaterMechanics : MonoBehaviour
 {
 
-    void Start()
+    [SerializeField] private GameObject _boundingBoxGO, _cameraGO;
+    [SerializeField] private Volume _postProcessingVolume;
+    [SerializeField] private Color _underWaterColor;
+
+    private bool _underWater = false;
+    private Vignette _vignette;
+    private DepthOfField _depthOfField;
+    private ColorAdjustments _colorAdjustments;
+
+
+    private void Start()
     {
-        
+        _postProcessingVolume.profile.TryGet(out _vignette);
+        _postProcessingVolume.profile.TryGet(out _depthOfField);
+        _postProcessingVolume.profile.TryGet(out _colorAdjustments);
     }
 
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (_boundingBoxGO.GetComponent<BoxCollider>().bounds.Contains(_cameraGO.transform.position))
+            _underWater = true;
+        else
+            _underWater = false;
+
+        if (_underWater)
+        {
+            _vignette.intensity.value = 0.35f;
+            _depthOfField.focusDistance.value = 1f;
+            _colorAdjustments.colorFilter.value = _underWaterColor ;
+        }
+        else {
+            _vignette.intensity.value = 0.292f;
+            _depthOfField.focusDistance.value = 5f;
+            _colorAdjustments.colorFilter.value = Color.white;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player") {
             Debug.Log("(In WaterMechanics.cs )Player dived into the water");
-            //Rigidbody playerRB = other.gameObject.GetComponent<Rigidbody>();
             other.GetComponent<Player>().inWater = true;
-
+            _underWater = true;
         }
     }
 }
